034f382f8d2ff9badce487c52e7cf54955224b7d
Andrey Zatonskiy <zaton.andrey@bk.ru>
2025-09-02 14:51:19 +0700

    Добавлены сущности и данные
    - Добавлены папка Data для хранения сущностей и данных
    - Добавлены сущности в папке Entities
    - Добавлено перечисление статуса задачи ProjectTaskStatus

---
+++ ProjectManagement/Data/Entities/ProjectTask.cs
@@ -0,0 +1,71 @@
+﻿namespace ProjectManagement.Data.Entities
+{
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+
+	/// <summary>
+	/// Represents a task within a project.
+	/// </summary>
+	public class ProjectTask
+	{
+		/// <summary>
+		/// Unique key of the task.
+		/// </summary>
+		[Key]
+		public int TaskId { get; set; }
+
+		/// <summary>
+		/// Name of the task.
+		/// </summary>
+		[Required, MaxLength(100)]
+		public string TaskName { get; set; }
+
+		/// <summary>
+		/// Current status of the task (default is ToDo).
+		/// </summary>
+		public ProjectTaskStatus Status { get; set; } = ProjectTaskStatus.ToDo;
+
+		/// <summary>
+		/// Priority level of the task.
+		/// </summary>
+		public int Priority { get; set; }
+
+		/// <summary>
+		/// Comment or description of the task.
+		/// </summary>
+		public string Comment { get; set; }
+
+		/// <summary>
+		/// Foreign key of the employee assigned to the task.
+		/// </summary>
+		[ForeignKey(nameof(Assignee))]
+		public int AssigneeId { get; set; }
+
+		/// <summary>
+		/// Employee assigned to execute the task.
+		/// </summary>
+		public Employee Assignee { get; set; }
+
+		/// <summary>
+		/// Foreign key of the employee who authored the task.
+		/// </summary>
+		[ForeignKey(nameof(Author))]
+		public int AuthorId { get; set; }
+
+		/// <summary>
+		/// Employee who created the task.
+		/// </summary>
+		public Employee Author { get; set; }
+
+		/// <summary>
+		/// Foreign key of the associated project.
+		/// </summary>
+		[ForeignKey(nameof(Project))]
+		public int ProjectId { get; set; }
+
+		/// <summary>
+		/// The project to which this task belongs.
+		/// </summary>
+		public Project Project { get; set; }
+	}
+}
---
+++ ProjectManagement/Data/ProjectTaskStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectManagement.Data
+{
+	/// <summary>
+	/// Defines the status of a project task.
+	/// </summary>
+	public enum ProjectTaskStatus
+	{
+		/// <summary>
+		/// Task is pending and not started.
+		/// </summary>
+		ToDo = 0,
+
+		/// <summary>
+		/// Task in progress.
+		/// </summary>
+		InProgress = 1,
+
+		/// <summary>
+		/// Task has been completed.
+		/// </summary>
+		Done = 2
+	}
+}
